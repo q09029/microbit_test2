1ac104f2b25b592ed84d1fc5a7c05e9f75f760ff
q09029 <q09029@shibaura-it.ac.jp>
2019-07-07 10:55:02 +0900

    Updates.

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
-// tests go here; this will not be compiled when this package is used as a library
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
+namespace comment {
+    //% blockId=show_strings block="コメント %v"
+    export function noaction(text: string): void {
+    }
+}
