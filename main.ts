5c768e75bb259fc0ba65c20e26ef4bd19ade5a8e
q09029 <q09029@shibaura-it.ac.jp>
2019-07-11 10:23:18 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -3,10 +3,7 @@ namespace comment {
     //% blockId=show_strings block="コメント %v"
     export function noaction(text: string): void {
     }
-}
-
-namespace comment2 {
     //% blockId=show_strings block="コメント2 %v"
-    export function noaction(text: string): void {
+    export function noaction2(text: string): void {
     }
 }
